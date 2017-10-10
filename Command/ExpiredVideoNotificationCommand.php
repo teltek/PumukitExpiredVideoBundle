bcea3fed2503bf6a67bdc5f3e94706d514ad4d84
pogando <pablo.ogando@teltek.es>
2017-10-10 13:49:49 +0200

    TTK-15626 add expiration key as condition to send emails

--- Command/ExpiredVideoNotificationCommand.php
+++ Command/ExpiredVideoNotificationCommand.php
@@ -71,7 +71,6 @@ EOT
      */
     private function findExpiredVideos($days, $range)
     {
-        $this->dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
         $mmobjExpired = $this->getExpiredVideos($days, $range);
 
         return $mmobjExpired;
@@ -89,14 +88,14 @@ EOT
         $date->add(new \DateInterval('P'.$days.'D'));
         $date = $date->format('Y-m-d H:i:s');
 
+        $qb = $this->mmobjRepo->createQueryBuilder()
+            ->field('properties.expiration_date')->exists(true)
+            ->field('properties.expiration_key')->exists(true);
+
         if ($range === 'false') {
             $oTomorrow = new \DateTime(date('Y-m-d'));
             $oTomorrow->add(new \DateInterval('P'.($days + 1).'D'));
             $oTomorrow = $oTomorrow->format('Y-m-d H:i:s');
-        }
-
-        $qb = $this->mmobjRepo->createQueryBuilder()->field('properties.expiration_date')->exists(true);
-        if ($range === 'false') {
             $qb->field('properties.expiration_date')->equals(array('$gte' => $date, '$lt' => $oTomorrow));
         } else {
             $qb->field('properties.expiration_date')->equals(array('$gte' => new \DateTime(date('Y-m-d')), '$lte' => $date));
@@ -111,12 +110,14 @@ EOT
      */
     private function sendNotification(OutputInterface $output, $aMultimediaObject)
     {
+        $sendMail = array();
         if ($aMultimediaObject) {
             foreach ($aMultimediaObject as $mmObj) {
                 $output->writeln('Expired Video ====> Multimedia Object ID - '.$mmObj->getId());
 
-                if (count($mmObj->getPeopleByRoleCod($this->user_code, true)) > 0) {
-                    foreach ($mmObj->getPeopleByRoleCod($this->user_code, true) as $person) {
+                $people = $mmObj->getPeopleByRoleCod($this->user_code, true);
+                if (count($people) > 0) {
+                    foreach ($people as $person) {
                         if ($person->getEmail()) {
                             $sendMail[$person->getId()]['videos'][] = $mmObj->getId();
                             $sendMail[$person->getId()]['email'] = $person->getEmail();
@@ -127,7 +128,7 @@ EOT
                 }
             }
 
-            if ($sendMail) {
+            if (!empty($sendMail)) {
                 try {
                     $this->expiredVideoService->generateNotification($sendMail, $this->type);
                 } catch (\Exception $e) {
