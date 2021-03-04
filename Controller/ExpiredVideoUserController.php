ea6860cd5f2c13af3ec930d04d9e3321d7b45763
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-03-04 11:09:38 +0100

    TTK-24954 refactor renew videos from email

--- Controller/ExpiredVideoUserController.php
+++ Controller/ExpiredVideoUserController.php
@@ -19,135 +19,56 @@ use Symfony\Component\HttpFoundation\Response;
 class ExpiredVideoUserController extends Controller implements NewAdminControllerInterface
 {
     /**
-     * @Route("/renew/{key}/", name="pumukit_expired_video_owner_renew",  defaults={"key": null})
+     * @Route("/renew/{key}/", name="pumukit_expired_video_owner_renew", defaults={"key": null})
      * @Template("PumukitExpiredVideoBundle:ExpiredVideo:renewExpiredVideo.html.twig")
      */
     public function renewMultimediaObjectFromEmailAction(string $key)
     {
-        $expiredVideoConfigurationService = $this->container->get('pumukit_expired_video.configuration');
-        $days = $expiredVideoConfigurationService->getExpirationDateDaysConf();
-
         if (!TokenUtils::isValidToken($key)) {
             return $this->redirectToRoute('homepage', [], Response::HTTP_MOVED_PERMANENTLY);
         }
 
-        $dm = $this->get('doctrine_mongodb.odm.document_manager');
-        $mmObj = $dm->getRepository(MultimediaObject::class)->findOneBy([
-            $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewKey(true) => new \MongoId($key),
-        ]);
+        $expiredVideoRenewService = $this->container->get('pumukit_expired_video.renew');
 
-        $user = $this->getUser();
-        $roleCode = $this->container->getParameter('pumukitschema.personal_scope_role_code');
-
-        if ($mmObj) {
-            $people = $mmObj->getPeopleByRoleCod($roleCode, true);
-            $isOwner = false;
-            if (isset($people) && !empty($people) && is_array($people)) {
-                foreach ($people as $person) {
-                    if ($person->getEmail() === $user->getEmail()) {
-                        $isOwner = true;
-                    }
-                }
-            }
-
-            if ($isOwner) {
-                $aRenew = $mmObj->getProperty(
-                    $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewExpirationDateKey()
-                );
-                $aRenew[] = $days;
-                $mmObj->setProperty(
-                    $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewExpirationDateKey(),
-                    $aRenew
-                );
-
-                $mmObj->removeProperty($expiredVideoConfigurationService->getMultimediaObjectPropertyRenewKey());
-
-                $date = new \DateTime();
-                $date->add(new \DateInterval('P'.$days.'D'));
-                $mmObj->setPropertyAsDateTime(
-                    $expiredVideoConfigurationService->getMultimediaObjectPropertyExpirationDateKey(),
-                    $date
-                );
-
-                $dm->flush();
-
-                $error = 0;
-            } else {
-                $error = 1;
-            }
-        } else {
-            $error = 2;
+        $multimediaObject = $expiredVideoRenewService->findVideoByRenewKey($key);
+        if (!$multimediaObject) {
+            return ['message' => 2];
         }
 
-        return ['message' => $error];
+        $isOwner = $expiredVideoRenewService->isOwner($multimediaObject, $this->getUser());
+        if (!$isOwner) {
+            return ['message' => 1];
+        }
+
+        $expiredVideoRenewService->renew($multimediaObject);
+
+        return ['message' => 0];
     }
 
     /**
-     * @Route("/all/renew/{key}/", name="pumukit_expired_video_owner_renew_all",  defaults={"key": null})
+     * @Route("/all/renew/{key}/", name="pumukit_expired_video_owner_renew_all", defaults={"key": null})
      * @Template("PumukitExpiredVideoBundle:ExpiredVideo:renewExpiredVideo.html.twig")
      */
     public function renewAllMultimediaObjectsFromEmailAction(string $key)
     {
-        $expiredVideoConfigurationService = $this->container->get('pumukit_expired_video.configuration');
-        $days = $expiredVideoConfigurationService->getExpirationDateDaysConf();
-
         if (!TokenUtils::isValidToken($key)) {
             return $this->redirectToRoute('homepage', [], Response::HTTP_MOVED_PERMANENTLY);
         }
 
-        $dm = $this->get('doctrine_mongodb.odm.document_manager');
-        $person = $dm->getRepository(Person::class)->findOneBy([
-            $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewKey(true) => new \MongoId($key),
-        ]);
+        $expiredVideoRenewService = $this->container->get('pumukit_expired_video.renew');
 
+        $person = $expiredVideoRenewService->getPersonWithRenewKey($key);
         if (!$person) {
-            $error = 4;
-
-            return ['message' => $error];
+            return ['message' => 4];
         }
 
-        $user = $this->getUser();
-        if ($user->getEmail() === $person->getEmail()) {
-            $aObject = $dm->getRepository(MultimediaObject::class)->findBy(
-                [
-                    'people.people._id' => $person->getId(),
-                    $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewKey(true) => ['$exists' => true],
-                ]
-            );
-
-            if (count($aObject) >= 1) {
-                foreach ($aObject as $mmObj) {
-                    $aRenew = $mmObj->getProperty(
-                        $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewExpirationDateKey()
-                    );
-                    $aRenew[] = $days;
-                    $mmObj->setProperty(
-                        $expiredVideoConfigurationService->getMultimediaObjectPropertyRenewExpirationDateKey(),
-                        $aRenew
-                    );
-
-                    $mmObj->removeProperty($expiredVideoConfigurationService->getMultimediaObjectPropertyRenewKey());
-
-                    $date = new \DateTime();
-                    $date->add(new \DateInterval('P'.$days.'D'));
-                    $mmObj->setPropertyAsDateTime(
-                        $expiredVideoConfigurationService->getMultimediaObjectPropertyExpirationDateKey(),
-                        $date
-                    );
-                }
-
-                $person->removeProperty(
-                    $expiredVideoConfigurationService->getMultimediaObjectPropertyExpirationDateKey()
-                );
-                $dm->flush();
-                $error = 0;
-            } else {
-                $error = 2;
-            }
-        } else {
-            $error = 1;
+        $multimediaObjects = $expiredVideoRenewService->findMultimediaObjectsByPerson($person);
+        if (!$multimediaObjects) {
+            return ['message' => 2];
         }
 
-        return ['message' => $error];
+        $expiredVideoRenewService->renewAllMultimediaObjects($multimediaObjects, $this->getUser());
+
+        return ['message' => 0];
     }
 }
